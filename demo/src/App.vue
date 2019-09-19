98df6bccb75c5a5ee71eaf64fe031ea239e721fa
hunite <hunite@striven.com>
2019-09-19 14:26:33 -0400

    aPP

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -60,7 +60,7 @@ export default {
     // paperPlane.innerHTML = "<svg width=\"25\" height=\"25\" viewBox=\"0 0 1792 1792\" xmlns=\"http://www.w3.org/2000/svg\"><path fill=\"#fff\" d=\"M1764 11q33 24 27 64l-256 1536q-5 29-32 45-14 8-31 8-11 0-24-5l-453-185-242 295q-18 23-49 23-13 0-22-4-19-7-30.5-23.5t-11.5-36.5v-349l864-1059-1069 925-395-162q-37-14-40-55-2-40 32-59l1664-960q15-9 32-9 20 0 36 11z\"/></svg>";
 
     const paperPlane = document.createElement('span');
-    const svg = "<svg width=\"25\" height=\"25\" viewBox=\"0 0 1792 1792\" xmlns=\"http://www.w3.org/2000/svg\">";
+    const svg = "<svg width=\"50%\" height=\"50%\" viewBox=\"0 0 1792 1792\" xmlns=\"http://www.w3.org/2000/svg\">";
     const path = "<path fill=\"#fff\" d=\"M1764 11q33 24 27 64l-256 1536q-5 29-32 45-14 8-31 8-11 0-24-5l-453-185-242 295q-18 23-49 23-13 0-22-4-19-7-30.5-23.5t-11.5-36.5v-349l864-1059-1069 925-395-162q-37-14-40-55-2-40 32-59l1664-960q15-9 32-9 20 0 36 11z\"/>";
     paperPlane.innerHTML = `${svg}${path}</svg>`;
 
