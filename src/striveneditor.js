9b74b02629a9c75c704d9f9ef3cc1347560ae560
unitehenry <unitehenry@gmail.com>
2019-10-11 12:51:54 -0400

    firefox fix

--- src/striveneditor.js
+++ src/striveneditor.js
@@ -73,6 +73,8 @@ const ACTIVEOPTIONCOLOR = "#ddd";
 
 export default class StrivenEditor {
     constructor(el, options) {
+        this.establishBrowser();
+
         this.range = new Range();
         this.files = [];
         this.optionGroups = OPTIONGROUPS;
@@ -197,7 +199,9 @@ export default class StrivenEditor {
                         }
                         break;
                     default:
-                        document.execCommand(command, true);
+                        if (this.isFirefox) { document.execCommand(command) }
+                        else { document.execCommand(command, true) }
+
                         this.toolbarState();
                         break;
                 }
@@ -1232,5 +1236,28 @@ export default class StrivenEditor {
                 }
             }
         })
+    }
+
+    establishBrowser() {
+        // Opera 8.0+
+        this.isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+
+        // Firefox 1.0+
+        this.isFirefox = typeof InstallTrigger !== 'undefined';
+
+        // Safari 3.0+ "[object HTMLElementConstructor]" 
+        this.isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+
+        // Internet Explorer 6-11
+        this.isIE = /*@cc_on!@*/false || !!document.documentMode;
+
+        // Edge 20+
+        this.isEdge = !this.isIE && !!window.StyleMedia;
+
+        // Chrome 1 - 71
+        this.isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+
+        // Blink engine detection
+        this.isBlink = (this.isChrome || this.isOpera) && !!window.CSS;
     }
 }
