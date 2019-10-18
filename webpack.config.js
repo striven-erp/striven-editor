4769eff969bfe684a2472887c15cd00da1a40c28
Omar Sood <omar.sood@onquality.de>
2019-10-18 21:18:52 +0200

    added optional react dependency, babel/preset-react for building react files

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-env'],
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
                         plugins: ['@babel/plugin-transform-modules-umd']
                     }
                 }
