06461302250c9a205747ab9c7b4dd8db6a47c3f2
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-10-24 15:21:00 +0200

    allow to pass initial content to editor

--- src/react-integration.jsx
+++ src/react-integration.jsx
@@ -1,8 +1,8 @@
 import StrivenEditor from "./striveneditor";
-import React, {useLayoutEffect, useRef, forwardRef} from "react";
+import React, {useLayoutEffect, useRef, useCallback, forwardRef} from "react";
 
 export function createEditor(options) {
-    return forwardRef((props, ref) => {
+    return forwardRef(({defaultContent}, ref) => {
         let container = useRef(null);
         /** @type {{current: StrivenEditor|null}}  **/
         let editor = useRef(null);
@@ -12,6 +12,9 @@ export function createEditor(options) {
                 editor.current = new StrivenEditor(container.current, options);
                 if (ref) {
                     ref.current = editor.current;
+                }
+                if (defaultContent !== undefined) {
+                    editor.current.setContent(defaultContent);
                 }
             },
             []
