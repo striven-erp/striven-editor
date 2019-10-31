aae01f0487a58435c0199121593b6caf96d021c5
unitehenry <unitehenry@gmail.com>
2019-10-31 13:21:54 -0400

    extension hot fix

--- src/striveneditor.js
+++ src/striveneditor.js
@@ -1085,7 +1085,7 @@ export default class StrivenEditor {
     }
 
     validateFile(file) {
-        const extension = (file && file.type.split('/').pop());
+        const extension = (file && file.name.split('.').pop());
         if (extension) {
             return this.options.extensions.includes(`.${extension}`);
         } else {
