ee07cc437be32a780fad12aaac76a48149c6afd5
unitehenry <unitehenry@gmail.com>
2020-07-16 10:38:43 -0400

    knockout will only initialize editor binding handler once

--- src/ko-striveneditor.js
+++ src/ko-striveneditor.js
@@ -1,77 +1,76 @@
 import StrivenEditor from './striveneditor';
 
-console.log('welcome');
-
 export default class KoStrivenEditor {
   constructor(ko, bindingName = 'striveneditor') {
-    
-    ko.bindingHandlers.striveneditor = {
-      init: function(
-        element,
-        valueAccessor,
-        allBindings,
-        viewModel,
-        bindingContext,
-      ) {
-        // Get options
-        let options = valueAccessor();
-        // Options must be passed as an observable
-        if (!ko.isObservable(options)) {
-          throw 'Options must be an observable.';
-        }
-
-        // Get the value binding in case we are binding the editor's contents to an observable
-        let value = allBindings().value;
-        if (value && !ko.isObservable(value)) {
-          throw 'Value must be an observable.';
-        }
-
-        // Pass the value to the editor in options
-        options().value = value();
-
-        let onChange = null;
-        let changeOpt = options().change;
-        if (changeOpt) {
-          if (ko.isObservable(changeOpt)) {
-            onChange = val => changeOpt(val);
-          } else {
-            onChange = changeOpt;
+    if(!ko.bindingHandlers.striveneditor) {
+      ko.bindingHandlers.striveneditor = {
+        init: function(
+          element,
+          valueAccessor,
+          allBindings,
+          viewModel,
+          bindingContext,
+        ) {
+          // Get options
+          let options = valueAccessor();
+          // Options must be passed as an observable
+          if (!ko.isObservable(options)) {
+            throw 'Options must be an observable.';
           }
-        }
 
-        let editor = new StrivenEditor(element, {
-          ...options(),
-          change: onChange,
-          toolbarTemplate:
-            options().toolbarTemplate && options().toolbarTemplate(),
-        });
-        let pauseUpdate = false;
+          // Get the value binding in case we are binding the editor's contents to an observable
+          let value = allBindings().value;
+          if (value && !ko.isObservable(value)) {
+            throw 'Value must be an observable.';
+          }
 
-        // Handle updates and changes to value observable
-        if (ko.isObservable(value)) {
-          // Listen to input event
-          editor.body.oninput = e => {
-            pauseUpdate = true;
-            value(editor.getContent());
-            pauseUpdate = false;
-          };
+          // Pass the value to the editor in options
+          options().value = value();
 
-          // Subscribe to the value observable to detect when it is changed and update the editor
-          value.subscribe(newValue => {
-            // Update the content if it's not being updated from within the editor
-            if (!pauseUpdate) {
-              editor.setContent(newValue);
+          let onChange = null;
+          let changeOpt = options().change;
+          if (changeOpt) {
+            if (ko.isObservable(changeOpt)) {
+              onChange = val => changeOpt(val);
+            } else {
+              onChange = changeOpt;
             }
+          }
+
+          let editor = new StrivenEditor(element, {
+            ...options(),
+            change: onChange,
+            toolbarTemplate:
+              options().toolbarTemplate && options().toolbarTemplate(),
           });
-        }
+          let pauseUpdate = false;
 
-        // Set the editor in options
-        options().api = editor;
+          // Handle updates and changes to value observable
+          if (ko.isObservable(value)) {
+            // Listen to input event
+            editor.body.oninput = e => {
+              pauseUpdate = true;
+              value(editor.getContent());
+              pauseUpdate = false;
+            };
 
-        ko.applyBindings(bindingContext, editor.toolbar);
-        return { controlsDescendantBindings: true };
+            // Subscribe to the value observable to detect when it is changed and update the editor
+            value.subscribe(newValue => {
+              // Update the content if it's not being updated from within the editor
+              if (!pauseUpdate) {
+                editor.setContent(newValue);
+              }
+            });
+          }
 
-      },
-   };
+          // Set the editor in options
+          options().api = editor;
+
+          ko.applyBindings(bindingContext, editor.toolbar);
+          return { controlsDescendantBindings: true };
+
+        },
+      };
+    }
   }
 }
